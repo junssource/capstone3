6e46aa37d01037aa03d184da518c321277937363
junssource <junsdesktop@gmail.com>
2020-03-19 16:18:19 +0900

    header.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" type="text/css" href="./css/main.css">
+<link rel="stylesheet" type="text/css" href="./css/common.css">
+<link rel="stylesheet" type="text/css" href="./css/responsive.css">
+
+<div id="title_box">
+	<a href="index.php"><span id="main_title">생활 패턴 계산기</span></a>
+	<div id="login_box">로그인</div>
+</div>
+<hr>
+
+
+개요 &nbsp;?<img src="rabbit.jpg">
+<div></div>
