336aaff9d479f0d65fc2158943a014c1d2fddae8
junssource <junsdesktop@gmail.com>
2020-03-19 16:18:50 +0900

    index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<head>
+<title>생활 패턴 계산기</title>
+<link rel="stylesheet" type="text/css" href="./css/responsive.css">
+<link rel="stylesheet" type="text/css" href="./css/common.css">
+<link rel="stylesheet" type="text/css" href="./css/main.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<style>
+	
+</style>
+</head>
+<body>
+	<div id="wrapper">
+		<header>
+			<?php include "header.php";?>
+		</header>
+		<section>
+			<?php include "main.php";?>
+		</section>
+		<footer>
+			<?php include "footer.php";?>
+		</footer>
+	</wrapper>
+</body>
+</html>
