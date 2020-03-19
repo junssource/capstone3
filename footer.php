d9deca98f4c63dd6ad1c9f65ccbca43138f3827f
junssource <junsdesktop@gmail.com>
2020-03-19 16:18:12 +0900

    footer.php

---
+++ footer.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" type="text/css" href="./css/main.css">
+<link rel="stylesheet" type="text/css" href="./css/common.css">
+<link rel="stylesheet" type="text/css" href="./css/responsive.css">
+
+<button>계산하기</button><br>
+제주대학교 캡스톤디자인3
