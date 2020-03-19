825d6b87dec4f1e50c91b5a871f54b0df15825bc
junssource <junsdesktop@gmail.com>
2020-03-19 16:18:55 +0900

    main.php

---
+++ main.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" type="text/css" href="./css/main.css">
+<link rel="stylesheet" type="text/css" href="./css/common.css">
+<link rel="stylesheet" type="text/css" href="./css/responsive.css">
+<form>
+유형 [버튼1][버튼2][버튼3][버튼4][버튼5][버튼6]<br>
+시간 <input type="text"><br>
+금액 <input type="text"><br>
+주기 <input type="text"><br>
+
+
+
+
+</form>
